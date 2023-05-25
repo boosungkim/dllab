5ab7df21a5517db812dd50746410a9bf44915366
boosungkim <boosung@boosungkim.com>
2023-05-25 23:49:46 +0900

    created resnet file

---
+++ models/resnet.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torchinfo import summary
+
+
+class CustomResidualBlock():
+    def __init__(self):
+        pass
+    
+    def create_block(self):
+        pass
+
+class CustomResNet34(nn.Module):
+    def __init__(self):
+        super(CustomResNet, self).__init__()
+
+    def forward(self):
+        pass
+
+    def create_residual_block(self):
+
+        pass
